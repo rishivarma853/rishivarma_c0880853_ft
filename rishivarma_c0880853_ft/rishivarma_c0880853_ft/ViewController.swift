a11339d67b34067f91461db202027fe4d853a1f6
Rishi Varma Kalidindi <rishivarma.lc.dev@gmail.com>
2022-11-07 19:13:21 -0500

    Created the UI Design

--- rishivarma_c0880853_ft/rishivarma_c0880853_ft/ViewController.swift
+++ rishivarma_c0880853_ft/rishivarma_c0880853_ft/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uiLabel_DisplayNumber: UILabel!
+    
+    @IBOutlet weak var uiLabel_Even: UILabel!
+    
+    @IBOutlet weak var uiLabel_Odd: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    func loadGame() {
+        uiLabel_DisplayNumber.text = Int.random(in: 1...<10000)
+    }
 
 }
 
